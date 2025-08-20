8388ebf42cee375baa89c340b6b055fada9cf5b9
Alejandro Mendoza <ayauri190106@gmail.com>
2025-08-20 13:57:05 -0500

    Añadir amigo a la lista de amigos y borrar campo y hacer las verificaciones necesarias

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    // Capturar el valor del campo de entrada
+    const nombre = document.getElementById("amigo").value.trim();
+
+    // Validar la entrada
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    //Actualizar el array de amigos
+    amigos.push(nombre);
+
+    // Limpiar el campo de entrada
+    nombre.value = "";
+
+ 
+
+    console.log(amigos);
+}
